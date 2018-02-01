6d9d56c38b48d6bf2c3248f3bb03ed899b542986
Denys Leonchev <dleo@softserveinc.com>
2018-02-01 18:48:04 +0200

    40 done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import express from 'express'
+import 'babel-polyfill'
+
 import renderer from './client/helpers/renderer'
+import createStore from './client/helpers/createStore'
 
 const app = express()
 
 app.use(express.static('public'))
 app.get('*', (req, res) => {
+  
+  const store = createStore()
 
-  res.send(renderer(req))
+  res.send(renderer(req, store))
 })
 
 app.listen(3000, () => {
