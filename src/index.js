41d83f965ec77fbe8cdc3d8fabb2801b98ecac7a
denleonchev <den.leonchev@gmail.com>
2018-02-04 23:48:32 +0200

    62 done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import 'babel-polyfill'
 import { matchRoutes } from 'react-router-config'
+import proxy from 'express-http-proxy'
 
 import renderer from './client/helpers/renderer'
 import createStore from './client/helpers/createStore'
@@ -8,10 +9,17 @@ import Routes from './client/Routes'
 
 const app = express()
 
+app.use('/api', proxy('http://react-ssr-api.herokuapp.com', {
+  //TODO test turning this off when OAuth is enabled
+  proxyReqOptDecorator(opts) {
+    opts.headers['x-forwarded-host'] = 'localhost:3000'
+    return opts
+  }
+}))
 app.use(express.static('public'))
 app.get('*',  (req, res) => {
   
-  const store = createStore()
+  const store = createStore(req)
   const promises =  matchRoutes(Routes, req.path).map(({ route }) => {
     return route.loadData ? route.loadData(store) : null
   })
